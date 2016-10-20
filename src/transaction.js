a7b45a1cb5f327e3fefb2fc2572cfc10bdc70019
Karel Bilek <kb@karelbilek.com>
2016-10-20 13:44:37 +0200

    Bug with non-JS transactions

--- src/transaction.js
+++ src/transaction.js
@@ -226,7 +226,7 @@ function scriptSize (someScript) {
 }
 
 Transaction.prototype.joinsplitByteLength = function () {
-  if (this.joinsplits == null) {
+  if (this.version < 2) {
     return 0
   }
 
